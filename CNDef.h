e03ec9271087935a055866154e963a52a4ff34db
giskook <zhangkai.gis@163.com>
2014-04-24 13:42:47 +0800

    "merge code"

--- CNDef.h
+++ CNDef.h
@@ -1,6 +1,22 @@
 #ifndef CNDEF_H_H
 #define CNDEF_H_H
 
+#ifndef IN
+#define IN
+#endif
+
+#ifndef OUT
+#define OUT
+#endif
+
+#if defined __GNUC__
+#define likely(x) __builtin_expect ((x),1)
+#define unlikely(x) __builtin_expect((x),0)
+#else
+#define likely(x)
+#define unlikely(x)
+#endif
+
 #define NSBEGIN namespace CETCNAV{
 #define NSEND }
 #define USECETCNAV using namespace CETCNAV;
